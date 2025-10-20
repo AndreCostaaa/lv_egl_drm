62aa0876708437b94be17e32f17283d247a95967
André <andre_miguel_costa@hotmail.com>
2025-10-20 11:17:47 +0200

    feat: use drm mode select cb

--- src/main.c
+++ src/main.c
@@ -4,35 +4,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
+static size_t drm_mode_select_cb(lv_display_t * disp, const lv_linux_drm_mode_t * modes, size_t mode_count)
 {
-	lv_init();
-	char *device = NULL;
-	if (argc < 2) {
-		device = "/dev/dri/card0";
-	} else {
-		device = argv[1];
-	}
+    LV_UNUSED(disp);
+    size_t preferred_mode = mode_count;
+    for(size_t i = 0; i < mode_count; i++) {
+        int32_t width = lv_linux_drm_mode_get_horizontal_resolution(&modes[i]);
+        int32_t height = lv_linux_drm_mode_get_vertical_resolution(&modes[i]);
+        int32_t refresh = lv_linux_drm_mode_get_refresh_rate(&modes[i]);
+        bool preferred = lv_linux_drm_mode_is_preferred(&modes[i]);
 
-	lv_display_t *disp = lv_linux_drm_create();
-	if (!disp) {
-		fprintf(stderr, "Failed to initialize drm\n");
-		return EXIT_FAILURE;
-	}
-	lv_linux_drm_set_file(disp, device, -1);
+        LV_LOG_USER("Mode %zu %dx%d@%d %d %d %d", i, width, height, refresh, preferred, modes[i].vsync_start,
+                    modes[i].vsync_end);
+        if(preferred) {
+            preferred_mode = i;
+        }
+    }
+    return preferred_mode;
+}
 
+
+int main(void)
+{
+    lv_init();
+    lv_display_t * disp = lv_linux_drm_create();
+    if(!disp) {
+        fprintf(stderr, "Failed to initialize drm\n");
+        return EXIT_FAILURE;
+    }
+    uint32_t start_tick = lv_tick_get();
+    char * device = lv_linux_drm_find_device_path();
+    lv_linux_drm_set_mode_cb(disp, drm_mode_select_cb);
+    lv_linux_drm_set_file(disp, device, -1);
+    LV_LOG_USER("DRM init time %u", lv_tick_get() - start_tick);
 #if LV_USE_DEMO_GLTF
-	lv_demo_gltf("A:../lv_gltf_viewer/gltfs/DiffuseTransmissionPlant.glb");
+    uint32_t demo_tick = lv_tick_get();
+    lv_demo_gltf("A:neo_virtual_city_touchup1.glb");
+    uint32_t end = lv_tick_get();
+    LV_LOG_USER("demo init time %u (%u)", end - demo_tick, end - start_tick);
 #else
-	lv_demo_benchmark();
+    lv_demo_benchmark();
 #endif
 
-	while (1) {
-		uint32_t time_until_next = lv_timer_handler();
-		if (time_until_next == LV_NO_TIMER_READY) {
-			time_until_next = LV_DEF_REFR_PERIOD;
-		}
-		usleep(time_until_next * 1000);
-	}
-	return 0;
+    while(1) {
+        uint32_t x = lv_timer_handler();
+        usleep(x * 1000);
+    }
+    lv_deinit();
+    return 0;
 }
